13efae39414e610fa1866645db76c2f639212acb
Harly <harly@AccenturedeMac-mini-2.local>
2014-12-22 16:29:58 +0800

    Initial Commit

---
+++ Health Beat/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Health Beat
+//
+//  Created by Harly on 14/12/22.
+//  Copyright (c) 2014年 Harly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
